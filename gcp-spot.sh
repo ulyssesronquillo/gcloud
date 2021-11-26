e34b0fa4310e2edfd058a68bc0c887d48447a65a
Ulysses <ulysses@penguin>
2021-11-26 17:51:40 -0500

    added gcp-spot.sh

---
+++ gcp-spot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+gcloud beta compute instances create spot-example \
+--provisioning-model=SPOT \
+--instance-termination-action=STOP \
+--image-project=rocky-linux-cloud \
+--image-family=rocky-linux-8 \
+--machine-type=e2-micro \
+--project=airy-totality-151318
